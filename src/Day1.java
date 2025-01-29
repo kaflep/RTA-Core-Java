5fc0e763fd56f39b079255d9f3389bce0047dcde
pratik Kafle <118851897+kaflep@users.noreply.github.com>
2025-01-29 16:16:32 -0600

    added new things

--- src/Day1.java
+++ src/Day1.java
@@ -2,5 +2,6 @@ public class Day1 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hi everyone !!");
+        System.out.println("please help me");
     }
 }
